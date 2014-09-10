803ff9c72fe10b6e95b415430a7cb1289e601d23
Anthony Palumbi <apalumbi@gmail.com>
2014-09-10 11:44:46 -0400

    first cracks to make sure I understood

---
+++ ZillowAPI/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ZillowAPI {
+	class Program {
+		static void Main(string[] args) {
+			var ZWSID = "X1-ZWz1b5hlrpbuob_2avji";
+			var address = Uri.EscapeUriString("527 Stanbridge Street");
+			var zip = Uri.EscapeUriString("19401");
+			var GetSearchResultsUrl = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id={0}&address={1}&citystatezip={2}";
+			var GetZestimateUrl = "http://www.zillow.com/webservice/GetZestimate.htm?zws-id={0}&zpid={1}&rentzestimate=true";
+			var GetDeepSearchResultUrl = "http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id={0}&address={1}&citystatezip={2}&rentzestimate=true";
+			//var searchUrl = string.Format(GetSearchResultsUrl, ZWSID, address, zip);
+			//var response = RunQuery(searchUrl);
+			//var zpid = GetZPID(response);
+			
+			//var zestimateUrl = string.Format(GetZestimateUrl, ZWSID, zpid);
+			//response = RunQuery(zestimateUrl);
+						
+			var deepSearchUrl = string.Format(GetDeepSearchResultUrl, ZWSID, address, zip);
+			var response = RunQuery(deepSearchUrl);
+
+			Console.WriteLine(response.ToString());
+			Console.ReadLine();
+
+		}
+
+		private static string GetZPID(XDocument doc) {
+			return new XMLParse().GetZPID(doc);
+		}
+
+		private static XDocument RunQuery(string searchUrl) {
+			return XDocument.Load(searchUrl);
+		}
+	}
+}
---
+++ ZillowAPITest/XMLParseTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.IO;
+using ZillowAPI;
+using System.Xml.Linq;
+
+namespace ZillowAPITest {
+	[TestFixture]
+	public class XMLParseTest {
+
+		[Test]
+		public void GetZPID() {
+			var xml = XDocument.Load("GetSearchResults.xml");
+
+			Assert.AreEqual("9369328", new XMLParse().GetZPID(xml));
+		}
+	}
+}
